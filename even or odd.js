d9f64a9617ea643d8fa73f78d9fa5dbaad59cdc8
smore1911 <smore@confederationcollege.ca>
2020-01-14 20:45:27 -0500

    Update even or odd.js

--- even or odd.js
+++ even or odd.js
@@ -1,5 +1,5 @@
 //https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
 function even_or_odd(number) {
-  return ( number & 1 ) ? "odd" : "even";
+  return ( number & 1 ) ? "Odd" : "Even";
 }
-console.log(even_or_odd(20))
+console.log(even_or_odd(2))
