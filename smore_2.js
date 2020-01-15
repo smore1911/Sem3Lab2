eca7415bfb81b8ead904683c43bd01e83e485ce9
smore1911 <smore@confederationcollege.ca>
2020-01-14 21:17:34 -0500

    Delete smore_2.js

--- smore_2.js
+++
@@ -1,9 +0,0 @@
-//https://www.codewars.com/kata/57a049e253ba33ac5e000212/train/javascript
-function factorial(n){
-  if (n == 0 || n == 1)
-    return 1;
-  if (factorial[n] > 0)
-    return factorial[n];
-  return factorial[n] = factorial(n-1) * n;
-}
-console.log(factorial(5))
