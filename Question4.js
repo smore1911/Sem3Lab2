686dc4103797d6dda1de327e8425df3edddacaf0
wolviex2411 <55698965+wolviex2411@users.noreply.github.com>
2020-01-17 20:03:07 -0500

    Delete Question4.js

--- Question4.js
+++
@@ -1,4 +0,0 @@
-//https://www.codewars.com/kata/5af15a37de4c7f223e00012d/train/javascript
-function menFromBoys(arr){
-  return [...new Set(arr.filter(v=>v%2===0).sort((a,b)=>a-b).concat(arr.filter(v=>v%2!==0).sort((a,b)=>b-a)))]
-}
