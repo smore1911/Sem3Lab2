b6795d716828ccf79fa4d179034b1c8b75bff218
wolviex2411 <55698965+wolviex2411@users.noreply.github.com>
2020-01-17 20:03:50 -0500

    Create Question1.js

---
+++ Aditya/Question1.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+function boolToWord( bool ){
+  if ( bool === true) {
+    return 'Yes'
+  } else {
+     return 'No'
+  }
+  
+}
+
+boolToWord(true) // 'Yes'
+boolToWord(false), 'No'
