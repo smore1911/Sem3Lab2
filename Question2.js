522edba7097ad728c3c53967541bd13398aece9b
wolviex2411 <55698965+wolviex2411@users.noreply.github.com>
2020-01-17 20:03:27 -0500

    Delete Question2.js

--- Question2.js
+++
@@ -1,4 +0,0 @@
-//https://www.codewars.com/kata/5bb904724c47249b10000131/train/javascript
-const points=games=>games.reduce((output,current)=>{
-    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
-  },0)
